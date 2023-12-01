3eeef9e973a27a31877b06925b5f843ab6f5b1fa
isabyte <isabelamigli@gmail.com>
2023-11-30 21:22:59 -0300

    actualizacion casi final

--- exbiblio.js
+++ exbiblio.js
@@ -1,15 +1,15 @@
 const generoLivro = {
-    FICCAO_CIENTIFICA : "Ficcao_cientifica.",
-    TERROR : "Terror.",
-    COMEDIA : "Comedia.",
-    SUSPENSE : "Suspense.",
-    DRAMA : "Drama.",
-    HISTORIA : "Historia.",
-    POLICIAL : "Policial."
+    FICCAO_CIENTIFICA: "Ficcao_cientifica.",
+    TERROR: "Terror.",
+    COMEDIA: "Comedia.",
+    SUSPENSE: "Suspense.",
+    DRAMA: "Drama.",
+    HISTORIA: "Historia.",
+    POLICIAL: "Policial."
 }
 
-class Usuario{
-    constructor(nome, registroAcademico, DataNascimento){
+class Usuario {
+    constructor(nome, registroAcademico, DataNascimento) {
         this.nome = nome;
         this.registroAcademico = registroAcademico;
         this.DataNascimento = DataNascimento;
@@ -17,71 +17,83 @@ class Usuario{
 }
 
 class Biblioteca {
-    constructor(){
+    constructor() {
         this.acervo = [];
         this.usuarios = [];
     }
 
-    popularAcervo(acervo){
-        acervo.forEach(item=>{
-            if(item.EntidadeBibliografica === "livro"){
+    popularAcervo(acervo) {
+        acervo.forEach(item => {
+            if (item.EntidadeBibliografica === "Livro") {
                 this.acervo.push(new Livro(item.codigo, item.titulo, item.autor, item.anoPublicacao, item.genero, item.isEmprestado, item.usuarioEmprestimo));
-            }else{
+            } else {
                 this.acervo.push(new Livro(item.codigo, item.titulo, item.autor, item.anoPublicacao, item.edicao, item.isEmprestado, item.usuarioEmprestimo));
             }
         });
     }
 
-    adicionarItem(item){
+    adicionarItem(item) {
         this.acervo.push(item);
         console.log("Item adicionado com succeso.");
     }
 
-    listarAcervo(){
-        if(this.acervo == []){
+    listarAcervo() {
+        if (this.acervo == []) {
             console.log("Acervo vazio.")
-        }else{
+        } else {
             this.acervo.forEach(item => {
-                console.log(item)
-            })
+                if (item.genero === "edicao") {
+                    console.log(`Titulo:${item.titulo}`);
+                    console.log(`Edicao: ${item.edicao}`);
+                    console.log(`Ano de publicacao:${item.anoPublicacao}`);
+                    console.log(`Autor:${item.autor}`);
+                    console.log(`Codigo:${item.codigo}`);
+                } else {
+                    console.log(`Titulo:${item.titulo}`);
+                    console.log(`Genero: ${item.genero}`);
+                    console.log(`Ano de publicacao:${item.anoPublicacao}`);
+                    console.log(`Autor:${item.autor}`);
+                    console.log(`Codigo:${item.codigo}`);
+                }
+            });
         }
     }
 
-    adicionarUsuario(usuario){
+    adicionarUsuario(usuario) {
         this.usuarios.push(usuario);
         console.log("Usuario adicionado com sucesso.")
     }
 
-    emprestarItem(codigo, registroAcademico){
+    emprestarItem(codigo, registroAcademico) {
         const item = this.acervo.find(item => item.codigo === codigo);
         const user = this.usuarios.find(user => user.registroAcademico === registroAcademico);
 
-        if(item){
-            if(user){
-                item.emprestar(usuario);
+        if (item) {
+            if (user) {
+                item.emprestar(user);
                 console.log("Emprestado com sucesso.");
-            }else{
+            } else {
                 console.log("Usuario nao encontrado.");
             }
-        }else{
+        } else {
             console.log("Entidade Bibliografica nao encontrada.");
         }
     }
 
-    devolverItem(codigo){
+    devolverItem(codigo) {
         const item = this.acervo.find(item => item.codigo === codigo)
-        if(item){
+        if (item) {
             item.isEmprestado = false;
             item.usuarioEmprestimo = null;
             console.log("Devolvido com sucesso")
-        }else{
+        } else {
             console.log("Item ja disponivel")
         }
     }
 }
 
-class EntidadeBibliografica{
-    constructor(codigo, titulo, autor, anoPublicacao){
+class EntidadeBibliografica {
+    constructor(codigo, titulo, autor, anoPublicacao) {
         this.codigo = codigo;
         this.titulo = titulo;
         this.autor = autor;
@@ -90,48 +102,45 @@ class EntidadeBibliografica{
         this.usuarioEmprestimo = null;
     }
 
-    emprestar(usuario){
-        if(this.isEmprestado == false){
+    emprestar(usuario) {
+        if (this.isEmprestado == false) {
             this.isEmprestado = true;
             this.usuarioEmprestimo = usuario;
-        }else{
+        } else {
             console.log("Entidade Bibliografica ja emprestada.")
         }
     }
 
-    devolver(){
-        if(this.isEmprestado == false){
+    devolver() {
+        if (this.isEmprestado == false) {
             console.log("Entidade Bibliografica ja devolvida.")
-        }else{
+        } else {
             this.isEmprestado == false;
             this.usuarioEmprestimo == null;
         }
     }
 }
 
-class Livro extends EntidadeBibliografica{
-    constructor(codigo, titulo, autor, anoPublicacao, genero){
+class Livro extends EntidadeBibliografica {
+    constructor(codigo, titulo, autor, anoPublicacao, genero) {
         super(codigo, titulo, autor, anoPublicacao);
         this.genero = genero;
     }
 
-    informacoes(){
+    informacoes() {
         console.log(`Codigo:${this.codigo}, Titulo:${this.titulo}, Autor:${this.autor}, Ano de Publicacao:${this.anoPublicacao}, Genero:${this.genero}`);
     }
 }
 
-class Revista extends EntidadeBibliografica{
-    constructor(codigo, titulo, autor, anoPublicacao, edicao){
-        super(codigo, titulo, autor, anoPublicacao, isEmprestado, usuarioEmprestimo);
+class Revista extends EntidadeBibliografica {
+    constructor(codigo, titulo, autor, anoPublicacao, edicao) {
+        super(codigo, titulo, autor, anoPublicacao);
         this.edicao = edicao;
     }
 
-    informacoes(){
+    informacoes() {
         console.log(`Codigo:${this.codigo}, Titulo:${this.titulo}, Autor:${this.autor}, Ano de Publicacao:${this.anoPublicacao}, Edicao:${this.edicao}`);
     }
 }
 
 
-const biblio = new Biblioteca();
-const livro1 = new Livro(1305,"señor de los anillos", "yo", 1980, generoLivro.COMEDIA);
-const user1 = new Usuario("tobias",505362,"05/11/2023");
